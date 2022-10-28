4e09d54d2b5f6b200cd6d7ac10a1c3c3217fda1c
Jeffrey Somarriba <31905398+J-Soma@users.noreply.github.com>
2022-10-28 14:56:16 -0600

    Agregado paquete django_extensions en proyecto

--- libros/settings.py
+++ libros/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
