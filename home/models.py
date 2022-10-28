a90235d4978bc73d36d52f8f505d8311a099b2b5
Jeffrey Somarriba <31905398+J-Soma@users.noreply.github.com>
2022-10-28 14:37:40 -0600

    Creado modelo Libro en home

--- home/models.py
+++ home/models.py
@@ -6,3 +6,13 @@ class Autor(models.Model):
 
 	def __str__(self):
 		return self.nombre
+
+class Libro(models.Model):
+	isbn = models.CharField(max_length=13, unique=True)
+	titulo = models.CharField(max_length=200)
+	fecha_pub = models.IntegerField()
+
+	autores = models.ManyToManyField(Autor, related_name='libros')
+
+	def __str__(self):
+		return f'{self.isbn} - {self.titulo}, {self.fecha_pub}'
