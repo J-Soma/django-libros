3f2937cf123c635f43debed2c79cfc3f57bb697f
Jeffrey Somarriba <31905398+J-Soma@users.noreply.github.com>
2022-11-04 15:33:49 -0600

    Modificado urls.py en home

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.index, name='index'),
 	path('login', views.iniciar_sesion, name='login'),
+	path('registro', views.registro, name='registro'),
+	path('logout', views.cerrar_sesion, name='logout'),
+	path('buscar', views.buscar, name='buscar'),
+	path('libro/<str:isbn>', views.libro, name='libro')
 ]
